1c58d39cc25f7c4be0e428205aca8d51e1799f4c
carrioneilnacr-art <carrioneil.nacr@gmail.com>
2026-04-14 20:41:31 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-// Esto permite leer el archivo .env en tu PC local
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root', 
-  password: process.env.DB_PASSWORD || '1234', 
-  database: process.env.DB_NAME || 'restaurante_wayra1',
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,    
-  connectionLimit: 10,         
-  queueLimit: 0                
-});
+// Si existe DATABASE_URL (en Render), la usa. Si no, usa el objeto de configuración (Local).
+const pool = process.env.DATABASE_URL 
+  ? mysql.createPool(process.env.DATABASE_URL)
+  : mysql.createPool({
+      host: process.env.DB_HOST || 'localhost',
+      user: process.env.DB_USER || 'root', 
+      password: process.env.DB_PASSWORD || '1234', 
+      database: process.env.DB_NAME || 'restaurante_wayra1',
+      port: process.env.DB_PORT || 3306,
+      waitForConnections: true,    
+      connectionLimit: 10,         
+      queueLimit: 0                
+    });
 
 export default pool;
